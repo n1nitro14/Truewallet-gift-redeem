016169f76f3bf23bef434addf96b93e6e2bdbac5
`leonard9892 <78474305+n1nitro14@users.noreply.github.com>
2021-08-15 12:51:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const truewallet = require('./truewallet')
 
+var linktw = 'https://gift.truemoney.com/campaign/?v=o4Xmn12jhkkejaffip'
+//กดรับซอง
+truewallet.pay('0800934345', linktw)
+  .then(async (res) => {
+  if (res.status === 'SUCCESS') {
+    console.log(res.status + 'You have recharged the amount' + res.amount)
+  } else {
+    console.log(res.status + 'Reason : ' + res.reason)
+  }
+}) //truewallet.pay(เบอร์ที่รับซอง, ลิ้งซองอั่งเปา)
